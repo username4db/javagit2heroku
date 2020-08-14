0f42ab25c538925c060233ccead8f7312f039090
admin <admin@vmubuntu>
2020-08-15 05:25:58 +0800

    refactor

---
+++ src/main/java/org/username4db/config/AppConfig.java
@@ -0,0 +1,31 @@
+package org.username4db.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class AppConfig {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
+
+}
