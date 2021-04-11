a5f8359114af01126751989a0a6329be7e5902a2
username4db <username4db@gmail.com>
2021-04-11 18:53:39 +0800

    open api 3

--- src/main/java/org/username4db/dto/api/ReqDTO.java
+++ src/main/java/org/username4db/dto/api/ReqDTO.java
@@ -2,19 +2,19 @@ package org.username4db.dto.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ReqDTO {
 
-	@Parameter(name = "@Parameter name", description = "@Parameter description", required = true)
+	@Schema(description = "@Schema description", example = " @Schema example ", required = true)
 	@JsonProperty(value = "INPUT-KEY", required = true)
 	private String key;
 
-	@Parameter(description = "value")
+	@Schema(description = "@Schema description", example = " @Schema example ", required = true)
 	@JsonProperty(value = "INPUT-VALUE", required = true)
 	private String value;
 
-	@Parameter(description = "caption")
+	@Schema(description = "@Schema description", example = " @Schema example ", required = true)
 	@JsonProperty(value = "INPUT-CAPTION", required = true)
 	private String valueCapition;
 
