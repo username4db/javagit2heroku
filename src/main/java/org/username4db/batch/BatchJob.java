f84659d7b8a2cb561080584aead0b6bf57fbe5b0
dev <dev@192.168.0.110>
2021-06-27 08:45:21 +0800

    remove restTemplate

--- src/main/java/org/username4db/batch/BatchJob.java
+++ src/main/java/org/username4db/batch/BatchJob.java
@@ -2,28 +2,25 @@ package org.username4db.batch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BatchJob {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private RestTemplate restTemp;
+//	@Autowired
+//	private RestTemplate restTemp;
 
 	@Scheduled(fixedRate = 60000)
 	public void timerRate() {
-		LOGGER.info("");
+		LOGGER.info("timerRate");
 	}
 
 	@Scheduled(cron = "00 */15 * * * ?")
 	public void timerCron() {
-		ResponseEntity<String> res = restTemp.getForEntity("https://javagit2heroku.herokuapp.com/", null);
-		LOGGER.info(res.getBody());
+//		ResponseEntity<String> res = restTemp.getForEntity("https://javagit2heroku.herokuapp.com/", null);
+		LOGGER.info("timer");
 	}
 }
